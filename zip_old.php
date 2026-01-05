6e75b3d37cb8a1f5851f2ada005f9a99cc0c8cd0
Nico Martin <mail@nico.dev>
2026-01-05 22:57:59 +0100

    updated matomo url

--- zip_old.php
+++ zip_old.php
@@ -3,7 +3,7 @@ require_once './src/MatomoTracker.php';
 
 const ABSPATH = __DIR__;
 
-$matomoTracker = new MatomoTracker((int)9, 'https://analytics.sayhello.agency/');
+$matomoTracker = new MatomoTracker((int)4, 'https://matomo.nico.dev/');
 $matomoTracker->disableCookieSupport();
 $matomoTracker->doTrackPageView('get plugin zip');
 
